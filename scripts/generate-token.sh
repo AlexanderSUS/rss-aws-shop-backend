8c5c834bcf799b117cceaba0f1ec292985f6abb7
alexandersus <sasha_suslov07@mail.ru>
2024-07-15 03:50:59 +0300

    fix: generate-token.sh

--- scripts/generate-token.sh
+++ scripts/generate-token.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ ! -f .env ]; then
-  echo ".env file does not exist"
+  echo ".env file does not exist. Run command:"
+  echo "cat env.example > .env"
   exit 1
 fi
 
 source .env
 
-echo "${GITHUB_ACCOUNT_LOGIN}:${AUTH_PASSWORD}" | base64
+printf "%s:%s" "$GITHUB_ACCOUNT_LOGIN" "$AUTH_PASSWORD" | base64
 
 
